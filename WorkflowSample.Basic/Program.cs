26159071b6e45c3bec96c3a9fa05349b8f43aace
Hideki Hashizume <hideki.hashizume@gmail.com>
2012-04-28 13:00:18 +0900

    Create Sample Applications of Windows Workflow Foundation

---
+++ WorkflowSample.Basic/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace WorkflowSample.Basic
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("◆実行したいワークフローを選択してください。");
+            Console.WriteLine("　1: Sequenceのサンプル");
+            Console.WriteLine("　2: Flowchartのサンプル");
+            Console.WriteLine("　3: StateMachineのサンプル");
+            Console.Write("何番を実行しますか？：");
+            var num = Console.ReadLine();
+            Console.WriteLine("");
+
+            Activity act;
+
+
+            if (num == "1")
+            {
+                act = new Sequence();
+            }
+            else if (num == "2")
+            {
+                act = new Flowchart();
+            }
+            else if (num == "3")
+            {
+                act = new StateMachine();
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+
+            //ワークフローの実行
+            WorkflowInvoker.Invoke(act);
+        }
+    }
+}
